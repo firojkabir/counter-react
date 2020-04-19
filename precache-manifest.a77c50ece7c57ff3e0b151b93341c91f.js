e6f53da62bc1ef1952cb5da67eef82a766b9aaa9
FirozKabir65 <firoj.kabir007@gmail.com>
2020-04-19 21:37:48 +0200

    Updates

---
+++ precache-manifest.a77c50ece7c57ff3e0b151b93341c91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0e4a36c60f2ccd17f276dc14295caf",
+    "url": "/counter-react/index.html"
+  },
+  {
+    "revision": "b7c7b7175bc8ee720f81",
+    "url": "/counter-react/static/css/main.dc5bb12b.chunk.css"
+  },
+  {
+    "revision": "d970b4fd4e06f9ae33d1",
+    "url": "/counter-react/static/js/2.70f80399.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-react/static/js/2.70f80399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c7b7175bc8ee720f81",
+    "url": "/counter-react/static/js/main.7264efb4.chunk.js"
+  },
+  {
+    "revision": "54598bf148d6dfe35c69",
+    "url": "/counter-react/static/js/runtime-main.232a9e65.js"
+  }
+]);
